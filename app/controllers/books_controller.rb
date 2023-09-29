f66cc5417639a4d6f14cd2a76605a9cb87419f0a
decknoboh <135430121+decknoboh@users.noreply.github.com>
2023-09-29 17:12:39 +0000

    bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,62 @@
 class BooksController < ApplicationController
-  def show
-  end
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def index
+    @books = Book.all
+    @user_list = current_user
+    @book_list = Book.new
+  end
+
+  def create
+    @book_list = Book.new(book_params)
+    @book_list.user_id = current_user.id
+    if @book_list.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book_list.id)
+    else
+      @books = Book.all
+      @user_list = current_user
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_list = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
 end
